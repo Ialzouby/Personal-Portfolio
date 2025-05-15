05df52282cb125cb2082d9402ffadbe751065989
ialzouby <ialzouby@gmail.com>
2025-05-15 00:08:48 -0400

    Update WhatDo.tsx

--- src/components/Pages/About/WhatDo.tsx
+++ src/components/Pages/About/WhatDo.tsx
@@ -43,7 +43,7 @@ const WhatDo = () => {
               </p>
             </div>
             <Link
-              href="/price"
+              href="/resume"
               className="p-btn bg1-color fw-medium n11-color px-3 px-md-6 py-2 py-md-4 rounded-pill d-flex align-items-center gap-2 h-100 text-nowrap"
             >
               <PiArrowRight /> My Experiences
