48de49ab4c3ab76aa95dd0a38572af9a359d8b80
ialzouby <ialzouby@gmail.com>
2026-01-27 00:35:37 -0500

    Update page.tsx

--- src/app/(WithLayout)/blog/page.tsx
+++ src/app/(WithLayout)/blog/page.tsx
@@ -11,25 +11,25 @@ import FadeDown from "@/components/motionEffect/FadeDown";
 const Blog = () => {
   return (
     <div>
-      <section className="banner-section-top pt-120 pb-60 mt-10 mt-lg-0">
+      <section className="banner-section-top pt-120 pb-60 mt-10 mt-lg-0 justify-content-center">
         <div className="banner-tint-overlay"></div>
         <FadeDown>
-          <div className="pb-60 br-bottom-n3">
+          <div className="pb-60 br-bottom-n3 w-100">
             <div className="container">
-              <div className="page-heading">
-                <h3 className="page-title fs-onefw-semibold n5-color mb-2 mb-md-3 text-center">
+              <div className="page-heading text-center d-flex flex-column align-items-center">
+                <h3 className="page-title fs-one fw-semibold n5-color mb-2 mb-md-3">
                   A Blog About Software Development And Life
                 </h3>
                 <p className="fs-seven n5-color mb-4 mb-md-8 text-center">
                   Welcome to my blog. Subscribe and get my latest blog post in
                   your inbox.
                 </p>
-                <div className="d-flex flex-wrap flex-sm-nowrap gap-3 gap-md-6">
+                <div className="d-flex flex-wrap flex-sm-nowrap gap-3 gap-md-6 justify-content-center">
                   <input
                     placeholder="Enter your email"
                     className="brn4 px-4 px-md-8 py-2 py-md-4 rounded-pill n5-color"
                   />
-                  <button className="p-btn bg1-color fw-medium n11-color px-3 px-md-6 py-2 py-md-4 rounded-pill d-flex align-items-center gap-2 mx-auto h-100 flex-shrink-0">
+                  <button className="p-btn bg1-color fw-medium n11-color px-3 px-md-6 py-2 py-md-4 rounded-pill d-flex align-items-center gap-2 h-100 flex-shrink-0">
                     <PiPaperPlaneTilt />
                     Subscribe
                   </button>
